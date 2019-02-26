82266d585a1dead9a9a66b9f88ac3cc4f72e6283
sharanreddy2393 <sharanreddy2393@gmail.com>
2019-02-26 16:33:07 +0530

    initial commit

---
+++ src/main/java/com/HallBooking/ownerService/OwnerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.HallBooking.ownerService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwnerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/HallBooking/ownerService/OwnerServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.HallBooking.ownerService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OwnerServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
